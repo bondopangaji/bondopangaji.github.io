f0ff5b9007428d3bc851d9f7b90768c97936def8
Bondo <bondopangaji@gmail.com>
2024-10-19 13:46:54 +0000

    feat: update what i do now

--- src/data/now.js
+++ src/data/now.js
@@ -1,10 +1,14 @@
 export const now = [
   {
     id: 1,
-    hnn: 'Learning Go with Echo'
+    hnn: 'Learning Golang'
   },
   {
     id: 2,
-    hnn: 'Playing around with Vue (Learning front-end paradigm)'
+    hnn: 'Playing around with Echo Framework'
+  },
+  {
+    id: 3,
+    hnn: 'Digging into Digital Research Analytic and IT in Finance'
   }
 ]
