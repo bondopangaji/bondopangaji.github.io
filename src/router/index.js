9ed444a4da6d30179f861865e7a5c6b87d9b0ab8
Bondo <bondopangaji@gmail.com>
2024-10-19 13:38:55 +0000

    fix: forgot the import

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Portfolio from '../views/Portfolio.vue'
 import Now from '../views/Now.vue'
-import About from '../views/About.vue'
+import Misc from '../views/Misc.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
