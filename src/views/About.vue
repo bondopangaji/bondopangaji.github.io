6ce471c9859d0e38f0c59a6c691da0f4b3d20292
Bondo <bondopangaji@gmail.com>
2025-03-13 05:07:37 +0000

    feat: update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,18 +1,18 @@
 <template>
-  <!-- <h3 class="title">Misc</h3>
+  <!-- <h3 class="title">About</h3>
   <p>
-    Hi! My name is Bondo Pangaji, A dedicated Student and Software Engineer currently based in
-    Indonesia, I enjoy writing code and building applications. In my recent years I've gained
-    valuable experience and absorbed knowledge from Senior Engineer at my former company, enriching
-    my skill set significantly.
+    I am <span class="bold">Bondo Pangaji</span>, a Software Developer with over 3 YoE in web development, specializing in backend development. 
+    Throughout my career, I have worked on a variety of projects, from designing and maintaining web applications for large-scale operations. 
+    I love writing clean, well-structured code and implementing best practices to build scalable and reliable solutions.
   </p>
-  
   <p>
-    Beyond my professional realm, You'll often see me play
-    <a href="https://playvalorant.com" target="_blank">VALORANT</a> and chilling with my friends. I
-    can speak English, Indonesian, and Javanese fluently. If you come accross me don't hesitate to
-    say hi! I'd absolutely love to connect!
-  </p> -->
+    In addition to my technical expertise, I have gained valuable experience collaborating with stakeholders and cross-functional teams to deliver impactful software. 
+    My experience in identifying and resolving technical issues has contributed to improved system performance and reliability.
+  </p>
+  <p>
+    Building on my technical foundation and problem-solving abilities, I am equipped to leverage data-driven insights to guide strategic decision-making and foster business growth.
+    My skill set allows me to contribute effectively in both software development and analytical capacities, delivering meaningful solutions across a range of challenges.
+  </p>
 
   <h3 class="title">What I use</h3>
   <ul>
