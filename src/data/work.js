b86d41d960724fae2f16a677ffd91bb7cf0e7bf1
Bondo <bondopangaji@gmail.com>
2025-03-13 05:39:07 +0000

    refactor: beats

--- src/data/work.js
+++ src/data/work.js
@@ -2,19 +2,17 @@ export const work = [
   {
     id: 4,
     title: 'Backend Developer - Jakarta (On-Site)',
-    companyName: 'IFG Life (MLK)',
+    companyName: 'IFG Life',
     companyLink: 'https://ifg-life.id/',
     companyDescription:
       'Work under Accord Innovations, one of the IFG Life vendors supporting payment division.',
     startDate: 'January 2025',
-    endDate: 'Now'
-    // responsibilities: [
-    //   `Designed, Developed and Maintained PTBA Rehandling's Dashboard Web Application`,
-    //   `Recreate and Improve PTBA Rehandling's Fleet Management System Web Application.`,
-    //   'Collaborate with stakeholders and developer throughout the development.',
-    //   'Identified and resolved software bugs, enhancing overall code quality and system reliability.',
-    //   'Support technical issue.'
-    // ]
+    endDate: 'Now',
+    responsibilities: [
+      `Enhance and Maintain IFG Life Payment Services`,
+      'Collaborate with Stakeholder, Developer and QA throughout the development.',
+      'Identify and solve software bugs, enhancing overall code quality and system reliability.',
+    ]
   },
   {
     id: 3,
@@ -26,10 +24,10 @@ export const work = [
     startDate: 'January 2024',
     endDate: 'December 2024',
     responsibilities: [
-      `Designed, Developed and Maintained PTBA Rehandling's Dashboard Web Application`,
-      `Recreate and Improve PTBA Rehandling's Fleet Management System Web Application.`,
-      'Collaborate with stakeholders and developer throughout the development.',
-      'Identified and resolved software bugs, enhancing overall code quality and system reliability.',
+      `Design, Develop and Maintain Rehandling Dashboard Web Application`,
+      `Reverse Engineer and Improve Rehandling Fleet Management System Web Application.`,
+      'Collaborate withSstakeholder and Developer throughout the development.',
+      'Identify and solve software bugs, enhancing overall code quality and system reliability.',
       'Support technical issue.'
     ]
   },
@@ -43,18 +41,16 @@ export const work = [
     startDate: 'August 2022',
     endDate: 'August 2023',
     responsibilities: [
-      'Maintaining Telin Enterprise and Telin Ticketing Web Application.',
+      'Maintain Telin Enterprise and Telin Ticketing Web Application.',
       'Build and implement REST API of Telin Enterprise and Telin Ticketing Web Application.',
-      'Collaborate with project managers and fellow developers throughout the RFC implementation to ensure the successful completion of the project.',
-      'Identified and resolved software bugs, enhancing overall code quality and system reliability.',
+      'Collaborate with Stakeholder and Devleoper throughout the RFC implementation to ensure the successful completion of the project.',
+      'Identify and solve software bugs, enhancing overall code quality and system reliability.',
       'Collaborate with project managers to effectively capture and articulate workflows in the business process documentation.',
-      'Participate code review with senior software engineer. Gained valuable feedback to improve my coding practices and skills.',
-      'Continuously learning and enhancing proficiency in programming abilities'
     ]
   },
   {
     id: 1,
-    title: 'Freelance Software Developer - Indonesia (On-Site)',
+    title: 'Freelance Software Developer - Central Java (On-Site)',
     companyName: 'Berkah Mulia Store',
     companyLink: '#',
     companyDescription:
@@ -63,7 +59,7 @@ export const work = [
     endDate: 'June 2022',
     responsibilities: [
       'Design, Develop and Maintain Internal Inventory Management for Internal User',
-      'Identified and resolved software bugs, enhancing overall code quality and system reliability.',
+      'Identify and solve software bugs, enhancing overall code quality and system reliability.',
       'Collaborate with store owner to effectively capture business process workflows in the development phase',
     ]
   }
